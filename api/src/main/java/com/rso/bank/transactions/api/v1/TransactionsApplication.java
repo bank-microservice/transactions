19a92c10553cddb2a5620d2a36dbabdac9b77160
bebari <rok.plevel@gmail.com>
2017-11-30 08:36:03 +0100

    service discovery, db fixes, added kubernetes scripts

--- api/src/main/java/com/rso/bank/transactions/api/v1/TransactionsApplication.java
+++ api/src/main/java/com/rso/bank/transactions/api/v1/TransactionsApplication.java
@@ -1,8 +1,11 @@
 package com.rso.bank.transactions.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@RegisterService
 public class TransactionsApplication extends Application {
 }
